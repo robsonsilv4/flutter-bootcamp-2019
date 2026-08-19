1b0d55af4bf4a32667dd1b4f1cfc36dffdaff715
robsonsilv4 <robsonsilv410@gmail.com>
2026-08-19 09:19:46 -0300

    feat: migrate i-am-rich to null safety and material 3

--- apps/i-am-rich/lib/main.dart
+++ apps/i-am-rich/lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const IAmRichApp());
+}
+
+class IAmRichApp extends StatelessWidget {
+  const IAmRichApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'I Am Rich',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+          brightness: Brightness.dark,
+        ),
+      ),
       home: Scaffold(
-    appBar: AppBar(
-      title: Text('I Am Rich'),
-      backgroundColor: Colors.blueGrey[900],
-    ),
-    backgroundColor: Colors.blueGrey,
-    body: Center(
-      child: Image(image: AssetImage('images/i_am_rich.gif')),
-    ),
-  )));
+        appBar: AppBar(title: const Text('I Am Rich')),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(image: AssetImage('images/i_am_rich.gif')),
+        ),
+      ),
+    );
+  }
 }
