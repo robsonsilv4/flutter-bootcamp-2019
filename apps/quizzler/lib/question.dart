b7ca62fe71f2e8f1626c36a855be835a3d013617
robsonsilv4 <robsonsilv410@gmail.com>
2026-08-19 22:37:52 -0300

    feat: migrate quizzler to null safety and fix score icons

--- apps/quizzler/lib/question.dart
+++ apps/quizzler/lib/question.dart
@@ -1,6 +1,6 @@
 class Question {
-  String question;
-  bool answer;
+  const Question(this.question, {required this.answer});
 
-  Question(this.question, this.answer);
+  final String question;
+  final bool answer;
 }
